cc35e837bda7e18bb10429f0b153ce23646409fc
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-08-17 09:45:14 +0200

    Created a reusable input

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,24 @@
+import React, { type ChangeEvent } from 'react'
+interface InputProps{
+    type?: string;
+    placeholder: string;
+    value: string;
+    onChange?:(e: ChangeEvent<HTMLInputElement>) => void;
+    classname?: string;
+    
+}
+
+export const Input:React.FC<InputProps> = ({type, placeholder, value, onChange}) => {
+  return (
+    <div className='w-full outline-none'> 
+        <input 
+          type= {type} 
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className='appearance-none outline-none'
+        />
+
+    </div>
+  )
+}
