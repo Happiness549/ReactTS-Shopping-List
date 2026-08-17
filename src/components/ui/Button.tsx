98f7b47fe26e419bee23c51b592c0f90546f5919
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-08-17 09:35:55 +0200

    Created a reusable button

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,36 @@
+
+interface ButtonProps{
+    text: string
+    onClick?: () => void;
+    className?: string;
+}
+
+export const Button:React.FC<ButtonProps> = ({text,onClick,className}) => {
+  return (
+    <div>
+        <button
+        
+        
+        onClick={onClick}
+        className={`
+            px-6
+            py-3
+            rounded-2xl
+            bg-[#2D99AE]
+            text-white
+            font-semibold
+            shadow-md
+            hover:bg-[#9ECAE1]
+            transition
+            duration-300
+            cursor-pointer
+            ${className || ''}
+            `}
+        
+        >
+            {text}
+        
+        </button>   
+    </div>
+  )
+}
