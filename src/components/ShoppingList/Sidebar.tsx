b6e510974b0fab403e9242c8d3ab4d66295f2c25
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-08-18 22:00:33 +0200

    Added icons of each item displayed on the dashboard and modified the styling

--- src/components/ShoppingList/Sidebar.tsx
+++ src/components/ShoppingList/Sidebar.tsx
@@ -1,16 +1,35 @@
 import {Card} from '../ui/Card'
 import {Text} from '../ui/Text'
 import { Link }from 'react-router-dom'
+import { HomeIcon } from 'lucide-react'
+import { ListChecksIcon } from 'lucide-react'
+import { TagsIcon } from 'lucide-react'
+import { SettingsIcon } from 'lucide-react'
+import { LogOutIcon } from 'lucide-react'
+import { ShoppingCartIcon } from 'lucide-react'
 
 export const Sidebar = () => {
 
     return(
         
     <div className='bg-[#001C44] min-h-screen w-50 rounded-l-4xl ml-5'>
-        <Text variant={'h1'} className='text-white text-4xl ml-10 mt-'>Shoply</Text>
-        <div className='flex flex-col ml-10 gap-10 mt-20 '>
+
+        
+            <Text variant={'h1'} className='text-white text-4xl ml-14 mt-10'>Shoply</Text>
+            <ShoppingCartIcon className='absolute text-white -mt-6 ml-4'/>
+
+            <div className='absolute text-white flex flex-col gap-10 mt-20 ml-4'>
+                <HomeIcon className='  '/>
+                <ListChecksIcon />
+                <SettingsIcon />
+                <TagsIcon/>
+                <LogOutIcon />
+            </div>
+        
+        
+        <div className='flex flex-col ml-14 gap-10 mt-20 '>
             <Link to='/Home'>
-             <Text variant={'p'} className='text-white'>Home</Text>
+             <Text variant={'p'} className='text-white '>Home</Text>
             </Link>
            
            <Link to=''>
