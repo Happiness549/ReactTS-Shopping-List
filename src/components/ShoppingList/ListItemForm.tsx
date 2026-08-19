2f93d98a7cb53066485ed95bf986b333e3e9822d
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-08-19 20:22:11 +0200

    Created a list item form

---
+++ src/components/ShoppingList/ListItemForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from '../ui/Text'
+import { Input } from '../ui/Input'
+
+export const ListItemForm = () => {
+  return (
+    <div>
+
+        
+    </div>
+  )
+}
